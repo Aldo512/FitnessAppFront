fd65de990001c23894213978df4f088038f7a25f
aldo512 <C.g242462536>
2022-08-07 17:20:51 +0200

    Created Api service

--- src/services/ApiService.ts
+++ src/services/ApiService.ts
@@ -1,10 +1,26 @@
 import axios from "axios";
 
-export const ApiService = () => {
-  return axios.create({
-    baseURL: "http://some-domain.com/api/",
-    timeout: 1000,
-  });
-};
+export const ApiService = axios.create({
+  baseURL: process.env.VUE_APP_API_URL,
+  withCredentials: true, // required to handle the CSRF token
+});
 
-// ApiService.get
+/*
+ * Add a response interceptor
+ */
+// authClient.interceptors.response.use(
+//   (response) => {
+//     return response;
+//   },
+//   function (error) {
+//     if (
+//       error.response &&
+//       [401, 419].includes(error.response.status) &&
+//       store.getters["auth/authUser"] &&
+//       !store.getters["auth/guest"]
+//     ) {
+//       store.dispatch("auth/logout");
+//     }
+//     return Promise.reject(error);
+//   }
+// );
